9ba5ff565e51210a4f3bb533e422f033b89b2b17
bryantonui <57979261+bryantonui@users.noreply.github.com>
2020-09-17 14:06:55 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <p>
+	<?php
+      echo "Hello user ";
+	?>
+</p>
+<a href="logout.php"><strong>Logout</strong></a>
+</body>
+</html>
