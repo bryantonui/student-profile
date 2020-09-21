f436810df51e53d71fbbabd5d519ee0f239b8937
bryantonui <57979261+bryantonui@users.noreply.github.com>
2020-09-21 11:14:12 +0300

    Update home.php

--- home.php
+++ home.php
@@ -9,6 +9,13 @@
       echo "Hello user ";
 	?>
 </p>
+<form >
+	<input type="text" class="input-field"  placeholder="Student Name"required>
+    	<input type="email" class="input-field"  placeholder="Email"required>
+    	<input type="text" class="input-field"  placeholder="Course"required>
+    	<input type="text" class="input-field"  placeholder="duration"required>
+</form>
+
 <a href="logout.php"><strong>Logout</strong></a>
 </body>
 </html>
